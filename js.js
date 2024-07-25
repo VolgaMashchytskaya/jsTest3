c20ec15d68d43f5c9bfbdc8937c88112fddcc99e
Volga <volga.mashchytskaya@gmail.com>
2024-07-25 15:11:44 +0300

    my commit

---
+++ js.js
@@ -0,0 +1,44 @@
+
+//document.getElementById("one").innerHTML = 'Hello World'
+//document.getElementById("one").innerHTML = '<h1>Anime</h1>'
+//document.querySelector("#one").innerHTML = '<h1>Anime2</h1>'
+//document.querySelector("h1").innerHTML = 'Anime3'
+//document.querySelector("p").innerHTML = 'Женева'
+
+//let btn = document.querySelector(".btn")
+//let inputIn = document.querySelector(".inp")
+//let div = document.querySelector(".out")
+//let mylist = document.querySelector(".outlist")
+
+
+//btn.onclick = function(){
+    //console.log("Это событие при клике!")
+    //let words = inputIn.value
+    //let e2 = +inputIn.value
+    //console.log(typeof(e))
+    //console.log(typeof(e2))
+    //mylist.innerHTML += `<li> ${words}</li>`
+
+//}
+
+//let mb = prompt('Как твое имя?')
+//console.log(mb)
+
+//if (mb === 'Люся'){
+//   alert('Привет, Люся!')
+//}
+//else{
+//   alert('Привет, незнакомый друг!')
+
+//}
+
+let number = prompt('Введите число')
+
+if (number == 100){
+    alert(number)
+}
+else{
+   console.log(number)
+}
+
+
